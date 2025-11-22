3ee5bf411bed1456329f8289ae6f0ac12a4dbeec
barletjb <barletjb.perso@gmail.com>
2025-11-22 16:39:05 +0100

    Feat: continue Service

--- src/main/java/com/example/analyzer/service/StudentAnalyzer.java
+++ src/main/java/com/example/analyzer/service/StudentAnalyzer.java
@@ -3,13 +3,12 @@ package com.example.analyzer.service;
 import com.example.analyzer.functional.StudentPredicate;
 import com.example.analyzer.model.Student;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StudentAnalyzer {
 
-    private List<Student> students;
+    private final List<Student> students;
 
     public StudentAnalyzer(List<Student> students) {
         this.students = students;
@@ -34,6 +33,43 @@ public class StudentAnalyzer {
                         .thenComparing(Student::getName))
                 .collect(Collectors.toList());
     }
+
+    public Map<String, List<Student>> groupByMajor() {
+        return students.stream()
+                .collect(Collectors.groupingBy(Student::getMajor));
+    }
+
+    public Map<String, List<Student>> groupByEmail() {
+        return students.stream()
+                .collect(Collectors.groupingBy(Student::getEmail));
+    }
+
+    public List<String> mapToEmails() {
+        return students.stream()
+                .map(Student::getEmail)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Student> findStudentById(String id) {
+        return students.stream()
+                .filter(student -> student.getId().equals(id))
+                .findFirst();
+    }
+
+    public DoubleSummaryStatistics gpaStatistics() {
+        return students.stream()
+                .collect(Collectors.summarizingDouble(Student::getGpa));
+    }
+
+    public Map<String, Double> averageGpaByMajor() {
+        return students.stream()
+                .collect(Collectors.groupingBy(Student::getMajor,
+                        Collectors.averagingDouble(Student::getGpa)));
+    }
+
+
+
+
 
 
 
